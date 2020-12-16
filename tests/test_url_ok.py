e5dd8a682a0884568208c981d06b8f66da1bc872
fasvan <voropaev.a.n@gmail.com>
2020-12-16 22:16:59 +0300

    Add Proxy

--- tests/test_url_ok.py
+++ tests/test_url_ok.py
@@ -4,16 +4,24 @@ import requests
 
 def test_get_homepage():
     r = requests.head("http://127.0.0.1:8989/")
-    assert r.status_code == 200
-
-def test_get_homepage_204():
-    r = requests.head("http://127.0.0.1:8989/")
     assert r.status_code == 204
 
-def test_get_homepage_3000_200():
+def test_get_proxy_3000():
     r = requests.head("http://127.0.0.1:3000/")
-    assert r.status_code == 200
+    assert r.status_code == 204
 
-def test_get_homepage_3001_204():
+def test_get_proxy_3001():
     r = requests.head("http://127.0.0.1:3001/")
+    assert r.status_code == 204
+
+def test_get_proxy_3002():
+    r = requests.head("http://127.0.0.1:3002/")
+    assert r.status_code == 204
+
+def test_get_proxy_3003():
+    r = requests.head("http://127.0.0.1:3003/")
+    assert r.status_code == 204
+
+def test_get_proxy_3004():
+    r = requests.head("http://127.0.0.1:3004/")
     assert r.status_code == 204
