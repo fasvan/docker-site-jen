06722cc0687b3479d9aff2e09523d4c9dc38a94a
fasvan <voropaev.a.n@gmail.com>
2020-12-16 21:52:02 +0300

    200,204

--- tests/test_url_ok.py
+++ tests/test_url_ok.py
@@ -5,3 +5,15 @@ import requests
 def test_get_homepage():
     r = requests.head("http://127.0.0.1:8989/")
     assert r.status_code == 200
+
+def test_get_homepage_204():
+    r = requests.head("http://127.0.0.1:8989/")
+    assert r.status_code == 204
+
+def test_get_homepage_3000_200():
+    r = requests.head("http://127.0.0.1:3000/")
+    assert r.status_code == 200
+
+def test_get_homepage_3001_204():
+    r = requests.head("http://127.0.0.1:3001/")
+    assert r.status_code == 204
